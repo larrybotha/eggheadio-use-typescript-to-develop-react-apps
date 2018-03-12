146d5e91203ccb250c8b1299cc50d4eef10050b5
Larry Botha <larry@fixate.it>
2018-03-12 09:00:05 +0100

    prepare app

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 
-// when creating stateless functional components it's recommended to annotate
-// components using the React.SFC interface
-// React.SFC takes a generic argument allowing one to specify the type annotation
-// for a component
-// The way the annotation is defined here is called an 'inline annotation'
-const App1: React.SFC<{message: string}> = ({message}) => <div>{message}</div>;
+class App extends React.Component {
+  render() {
+    return <div>hello world</div>;
+  }
+}
 
-// Inline annotations can be moved out using type annotations:
-type App2Props = {message: string};
-const App2: React.SFC<App2Props> = ({message}) => <div>{message}</div>;
-
-render(
-  <div>
-    <App1 message="hello world 1" />
-    <App2 message="hello world 2" />
-  </div>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
