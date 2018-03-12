b60b480ffb8ead65ac40dae055e8adae792a71cd
Larry Botha <larry@fixate.it>
2018-03-12 08:56:57 +0100

    annotate type annotations, inline, and abstracted

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 
+// when creating stateless functional components it's recommended to annotate
+// components using the React.SFC interface
+// React.SFC takes a generic argument allowing one to specify the type annotation
+// for a component
+// The way the annotation is defined here is called an 'inline annotation'
+const App1: React.SFC<{message: string}> = ({message}) => <div>{message}</div>;
+
+// Inline annotations can be moved out using type annotations:
+type App2Props = {message: string};
+const App2: React.SFC<App2Props> = ({message}) => <div>{message}</div>;
 
 render(
-  <Hello compiler="Typescript" framework="React" />,
+  <div>
+    <App1 message="hello world 1" />
+    <App2 message="hello world 2" />
+  </div>,
   document.getElementById('root')
 );
