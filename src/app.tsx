d664291b3852f9fe87d32e34b8719acce91a0c01
Larry Botha <larry@fixate.it>
2018-03-13 09:27:00 +0100

    prepare app

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,8 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 
-type AppProps = {message: string};
-type AppState = {count: number};
+import {CheckboxWithLabel} from './CheckboxWithLabel';
 
-// In typescript React.Component takes 2 annotations,
-// the first for props, and the second for state
-// class App extends React.Component<{message: string}, {count: number}> {
-// Annotations can also be abstracted using the 'type' keyword
-class App extends React.Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
+const App = () => <CheckboxWithLabel labelOn="on" labelOff="off" />;
 
-    this.state = {count: 0};
-  }
-
-  handleIncrement = () => {
-    this.setState(({count}) => ({count: count + 1}));
-  };
-
-  render() {
-    const {message} = this.props;
-    const {count} = this.state;
-
-    return (
-      <div onClick={this.handleIncrement}>
-        {message} {count}
-      </div>
-    );
-  }
-}
-
-render(<App message="hello world" />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
