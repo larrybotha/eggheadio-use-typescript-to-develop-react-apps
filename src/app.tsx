5f10f607fb378639dbe14d95e9bbfc10868f8578
Larry Botha <larry@fixate.it>
2018-03-13 09:08:26 +0100

    add notes on annotating React.Component

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
+type AppProps = {message: string};
+type AppState = {count: number};
+
+// In typescript React.Component takes 2 annotations,
+// the first for props, and the second for state
+// class App extends React.Component<{message: string}, {count: number}> {
+// Annotations can also be abstracted using the 'type' keyword
+class App extends React.Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {count: 0};
+  }
+
+  handleIncrement = () => {
+    this.setState(({count}) => ({count: count + 1}));
+  };
+
   render() {
-    return <div>hello world</div>;
+    const {message} = this.props;
+    const {count} = this.state;
+
+    return (
+      <div onClick={this.handleIncrement}>
+        {message} {count}
+      </div>
+    );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App message="hello world" />, document.getElementById('root'));
